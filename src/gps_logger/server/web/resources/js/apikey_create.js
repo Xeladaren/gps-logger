454ddc211692a7230afe1b88973e32bc59009fa8
Xeladaren <xeladaren@pm.me>
2025-03-16 15:24:21 +0100

    update apikey_create js.
    - add now min date on 'expire-date' input
    - set attribute required on 'expire-date'if 'expire' is checked.

--- src/gps_logger/server/web/resources/js/apikey_create.js
+++ src/gps_logger/server/web/resources/js/apikey_create.js
@@ -4,16 +4,20 @@ timezoneElem.setAttribute("value", timezoneVal);
 
 const checkboxElem = document.getElementById("expire");
 const expireDateElem = document.getElementById("expire-date");
+const date = new Date();
+const date_str_local = date.toLocaleString('sv', {year:'numeric', month:'numeric', day:'numeric', hour:'numeric', minute:'numeric'}).replace(' ', 'T');
+
+expireDateElem.setAttribute('min', date_str_local)
 
 checkboxElem.addEventListener('change', (event) => {
     if (event.currentTarget.checked)
     {
-        console.log("expire checked")
         expireDateElem.removeAttribute("disabled")
+        expireDateElem.setAttribute("required", "")
     }
     else
     {
-        console.log("expire unchecked")
         expireDateElem.setAttribute("disabled", "")
+        expireDateElem.removeAttribute("required")
     }
 })
